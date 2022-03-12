eb8c4ecfba9df35f4eaf978ad79386d494e57833
seher <96298352+SeherKayacan@users.noreply.github.com>
2022-03-12 16:49:06 +0300

    -

---
+++ hw-2.js
@@ -0,0 +1,7 @@
+//-----------------------------------------
+Array.prototype.includesCi=function(caseins){
+          
+    const even=(value)=>value.toLowerCase()===caseins.toLowerCase()
+    return this.some(even)
+}
+console.log(['admin','guest','AK'].includesCi('ak'))
